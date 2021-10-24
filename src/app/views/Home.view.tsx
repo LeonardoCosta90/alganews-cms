67225379a3026f4492bb32d7cd7ed56510a50a0f
Leonardo Costa <leonardo.costa@toodoo.com.br>
2021-10-23 22:07:57 -0300

    feat: firts features

--- src/app/views/Home.view.tsx
+++ src/app/views/Home.view.tsx
@@ -1,12 +1,13 @@
 import usePageTitle from "../../core/hooks/usePageTitle"
+import PostList from "../features/PostsList"
+import UserMetrics from "../features/UserMetrics"
 import DefaultLayout from "../layouts/Default"
 
 export default function Home () {
   usePageTitle('Home')
+
   return <DefaultLayout>
-    <h1>Home</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse incidunt eligendi iure ut rerum, distinctio quisquam sed culpa minima ipsa.
-    </p>
+    <UserMetrics />
+    <PostList />
   </DefaultLayout>
 }
